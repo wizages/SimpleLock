629bd07f77dfc81a6c4f47bf4078f0e238873107
Sam Patzer <wizagesmax@gmail.com>
2014-11-26 20:43:19 -0700

    Some uncommitted changes that fix issues
    Fixes height issue with the phone on convergence and changes the
    settings a little bit

--- Config.js
+++ Config.js
@@ -2,9 +2,9 @@ var tempUnit = "f"; // Replace with c for celcuis.
 var hourConfig = false; // Turn on for 24hr.
 var WOEID = "12783042"; // Weather code. Just google WOEID
 var textColor = "black"; // Change color of text. Enter a color name, hex, or rgba.
-var fontSize = 100;
-var refreshRate = 1; //Change refresh rate of weather (hours)
-var ampmVisable = true; //Turns am and pm visablity on and off
-var autoAnimate = true; //Turns on auto animation so no more tapping
-var animationLength = 15; //Time in sec for animation to take place
-var animationPause = 1; //Time in sec for pause in between auto animations (only works with autoAnimate = true)
+var fontSize = 70;
+var refreshRate = 1; // Change refresh rate of weather (hours)
+var ampmVisable = true; // Turns am and pm visablity on and off
+var autoAnimate = true; // Turns on auto animation so no more tapping
+var animationLength = 2; // Time in sec for animation to take place
+var animationPause = 1;
